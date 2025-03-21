0a8f8573f6b678cf4a28039fb64d5e95740d0c6c
Chase Vickery <chasedvickery@gmail.com>
2025-03-20 20:45:16 -0400

    Get Human Input

--- game.js
+++ game.js
@@ -1,10 +1,24 @@
 
+let humanScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice() {
     let choiceNumber = Math.floor(Math.random() * 3);
     if (choiceNumber === 0) { return "rock"; }
     else if (choiceNumber === 1) { return "paper"; }
     else { return "scissors"; }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Choose rock, paper, or scissors");
+    return choice;
+}
+
+function isValidChoice(choice) {
+    return  choice === "rock" ||
+            choice === "paper" ||
+            choice === "scissors";
 }
 
 // Test random choice
@@ -19,4 +33,11 @@ for (let i = 0; i < total; i++) {
     else if (choice === "scissors") { s++; }
 }
 console.log(`Rock:${100*r/total}%\nPaper:${100*p/total}%\nScissors:${100*s/total}%\n`);
+*/
+
+// Test user choice
+/*
+let c;
+c = getHumanChoice();
+console.log(`You chose '${c}' which is ${isValidChoice(c) ? '' : 'not'} valid.`);
 */
